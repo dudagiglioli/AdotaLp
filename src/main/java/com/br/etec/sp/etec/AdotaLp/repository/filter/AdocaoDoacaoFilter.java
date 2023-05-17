109200acf482aaefa5863517f94bc0af8c0652d7
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-17 07:45:55 -0300

    Filter Finished

---
+++ src/main/java/com/br/etec/sp/etec/AdotaLp/repository/filter/AdocaoDoacaoFilter.java
@@ -0,0 +1,50 @@
+package com.br.etec.sp.etec.AdotaLp.repository.filter;
+
+public class AdocaoDoacaoFilter {
+
+    private String nomedoador;
+    private String nomeadotante;
+    private String nomeanimal;
+    private String nomecidade;
+    private String sigla;
+
+    public String getNomedoador() {
+        return nomedoador;
+    }
+
+    public void setNomedoador(String nomedoador) {
+        this.nomedoador = nomedoador;
+    }
+
+    public String getNomeadotante() {
+        return nomeadotante;
+    }
+
+    public void setNomeadotante(String nomeadotante) {
+        this.nomeadotante = nomeadotante;
+    }
+
+    public String getNomeanimal() {
+        return nomeanimal;
+    }
+
+    public void setNomeanimal(String nomeanimal) {
+        this.nomeanimal = nomeanimal;
+    }
+
+    public String getNomecidade() {
+        return nomecidade;
+    }
+
+    public void setNomecidade(String nomecidade) {
+        this.nomecidade = nomecidade;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+}
