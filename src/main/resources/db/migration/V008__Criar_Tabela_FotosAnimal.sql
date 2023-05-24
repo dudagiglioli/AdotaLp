ebdc4bc521ed2ed96af9a3862846205e0b1a3228
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-24 08:44:24 -0300

    V008 finished

---
+++ src/main/resources/db/migration/V008__Criar_Tabela_FotosAnimal.sql
@@ -0,0 +1,9 @@
+create table fotosanimal(
+    id int not null primary key auto_increment,
+    idanimal int not null,
+    foto1 varchar(70),
+    foto2 varchar(70),
+    foto3 varchar(70),
+    foto4 varchar(70),
+    foto5 varchar(70)
+);
