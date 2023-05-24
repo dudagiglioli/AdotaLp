2fa8cb1b0fb65504e860da3612e2cb54191694c9
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-24 08:32:25 -0300

    FotosAnimal FK Finish

--- src/main/java/com/br/etec/sp/etec/AdotaLp/model/FotosAnimal.java
+++ src/main/java/com/br/etec/sp/etec/AdotaLp/model/FotosAnimal.java
@@ -1,5 +1,90 @@
 package com.br.etec.sp.etec.AdotaLp.model;
 
+import javax.persistence.*;
+import java.util.Objects;
 
+@Entity
+@Table(name = "fotsanimal")
 public class FotosAnimal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String foto1;
+    private String foto2;
+    private String foto3;
+    private String foto4;
+    private String foto5;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFoto1() {
+        return foto1;
+    }
+
+    public void setFoto1(String foto1) {
+        this.foto1 = foto1;
+    }
+
+    public String getFoto2() {
+        return foto2;
+    }
+
+    public void setFoto2(String foto2) {
+        this.foto2 = foto2;
+    }
+
+    public String getFoto3() {
+        return foto3;
+    }
+
+    public void setFoto3(String foto3) {
+        this.foto3 = foto3;
+    }
+
+    public String getFoto4() {
+        return foto4;
+    }
+
+    public void setFoto4(String foto4) {
+        this.foto4 = foto4;
+    }
+
+    public String getFoto5() {
+        return foto5;
+    }
+
+    public void setFoto5(String foto5) {
+        this.foto5 = foto5;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FotosAnimal that = (FotosAnimal) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    //--------------------------------------------------------------------------------------------------------------
+
+    @ManyToOne
+    @JoinColumn(name = "idanimal")
+    private Animal fotosanimal;
+
+    public Animal getFotosanimal() { return fotosanimal; }
+
+    public void setFotosanimal(Animal fotosanimal) { this.fotosanimal = fotosanimal; }
 }
