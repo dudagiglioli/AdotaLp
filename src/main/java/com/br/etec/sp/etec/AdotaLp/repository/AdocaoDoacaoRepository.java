99bce142cf2b09b5245580b868c2e07e3a4147e2
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-29 09:33:56 -0300

    AdocaoDoacaoRepository Update

--- src/main/java/com/br/etec/sp/etec/AdotaLp/repository/AdocaoDoacaoRepository.java
+++ src/main/java/com/br/etec/sp/etec/AdotaLp/repository/AdocaoDoacaoRepository.java
@@ -1,9 +1,10 @@
 package com.br.etec.sp.etec.AdotaLp.repository;
 
 import com.br.etec.sp.etec.AdotaLp.model.AdocaoDoacao;
+import com.br.etec.sp.etec.AdotaLp.repository.adocaodoacao.AdocaoDoacaoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdocaoDoacaoRepository extends JpaRepository<AdocaoDoacao, Integer>{
+public interface AdocaoDoacaoRepository extends JpaRepository<AdocaoDoacao, Integer>, AdocaoDoacaoRepositoryQuery {
 }
