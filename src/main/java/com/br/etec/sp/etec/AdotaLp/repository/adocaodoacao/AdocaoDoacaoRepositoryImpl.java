c6b2491984fd1db37f3625bdf741202175211805
MelloNickolas <nickolasphmello10@gmail.com>
2023-05-29 09:21:53 -0300

    Filtrar Finished...

--- src/main/java/com/br/etec/sp/etec/AdotaLp/repository/adocaodoacao/AdocaoDoacaoRepositoryImpl.java
+++ src/main/java/com/br/etec/sp/etec/AdotaLp/repository/adocaodoacao/AdocaoDoacaoRepositoryImpl.java
@@ -1,5 +1,36 @@
 package com.br.etec.sp.etec.AdotaLp.repository.adocaodoacao;
 
+import com.br.etec.sp.etec.AdotaLp.model.AdocaoDoacao;
+import com.br.etec.sp.etec.AdotaLp.repository.filter.AdocaoDoacaoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 public class AdocaoDoacaoRepositoryImpl {
 
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public Page<AdocaoDoacao> Filtrar(AdocaoDoacaoFilter adocaodoacaofilter, Pageable pageable) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<AdocaoDoacao> criteria = builder.createQuery(AdocaoDoacao.class);
+        Root<AdocaoDoacao> root = criteria.from(AdocaoDoacao.class);
+
+        Predicate[] predicates = criarrestricoes(adocaodoacaofilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.desc(root.get("data")));
+
+        TypedQuery<AdocaoDoacao> query = manager.createQuery(criteria);
+
+        return  new PageImpl<>(query.getResultList(), pageable, total(adocaodoacaofilter));
+    }
 }
