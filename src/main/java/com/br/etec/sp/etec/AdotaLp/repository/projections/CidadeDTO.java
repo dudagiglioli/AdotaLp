bfce7a5f5e27a6bb068a957dd27dc95bc201e4f7
MelloNickolas <nickolasphmello10@gmail.com>
2023-07-27 08:08:44 -0300

    Update CidadeDTO.java

--- src/main/java/com/br/etec/sp/etec/AdotaLp/repository/projections/CidadeDTO.java
+++ src/main/java/com/br/etec/sp/etec/AdotaLp/repository/projections/CidadeDTO.java
@@ -1,4 +1,48 @@
 package com.br.etec.sp.etec.AdotaLp.repository.projections;
 
 public class CidadeDTO {
+
+    private Integer id;
+    private String nome;
+    private String nomestado;
+    private String sigla;
+
+    public CidadeDTO(Integer id, String nome, String nomestado, String sigla) {
+        this.id = id;
+        this.nome = nome;
+        this.nomestado = nomestado;
+        this.sigla = sigla;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomestado() {
+        return nomestado;
+    }
+
+    public void setNomestado(String nomestado) {
+        this.nomestado = nomestado;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
 }
